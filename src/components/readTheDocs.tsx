078d70c189993c7d0d4baa8b3b4cbaf3316a87a8
Ben Feist <bf@benfeist.com>
2023-09-17 12:41:20 -0400

    css modules and test component

---
+++ src/components/readTheDocs.tsx
@@ -0,0 +1,11 @@
+import styles from "./readTheDocs.module.css";
+
+const ReadTheDocs = () => {
+  return (
+    <p className={styles.readTheDocs}>
+      Click on the Vite and React logos to learn more
+    </p>
+  );
+};
+
+export default ReadTheDocs;
